8738cfaa1c0588ff9ca7fb487112249317544cf6
ejolly <eshin.jolly@gmail.com>
2020-10-12 15:56:35 -0400

    Added a cacheing decorator for saving long run functions to disk.

--- utilz/guards.py
+++ utilz/guards.py
@@ -9,6 +9,10 @@ Custom guard decorators compatible with bulwark
 
 from functools import wraps
 import datetime as dt
+import pandas as pd
+import numpy as np
+import deepdish as dd
+from pathlib import Path
 
 
 def log_df(func):
@@ -19,6 +23,7 @@ def log_df(func):
         func (callable): a function that operates on a dataframe
 
     """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         tic = dt.datetime.now()
@@ -28,3 +33,52 @@ def log_df(func):
         return result
 
     return wrapper
+
+
+def disk_cache(threshold=60, autoload=True, index=False, verbose=False):
+    """
+    Save the result of a function to disk if it takes longer than delta to run. Then on subsequent runs given the same arrangement of args and kwargs, first try to load the last result and return that, rather than rerunning the function.
+
+    Args:
+        func (callable): a function to run
+        threshold (int, optional): threshold in seconds over which object is saved to disk. Defaults to 60.
+        autoload (bool, optional): whether to try to load a previously persisted result if all args and kwargs match in a subsequent function call. Default to True;
+        index (bool; optional): whether to incluce the index when saving a dataframe. Default to False
+        verbose (bool; optional): print debug messages; Default to False
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            filtered_args = [
+                e for e in args if not isinstance(e, (list, pd.DataFrame, np.ndarray))
+            ]
+            key = str((filtered_args, tuple(sorted(kwargs.items()))))
+            key_csv = f"{func.__name__}_{key}.csv"
+            key_h5 = f"{func.__name__}_{key}.h5"
+            if autoload:
+                if Path(key_csv).exists():
+                    if verbose:
+                        print("Returning cached result")
+                    return pd.read_csv(key_csv)
+                elif Path(key_h5).exists():
+                    if verbose:
+                        print("Returning cached result")
+                    return dd.io.load(key_h5)
+            if verbose:
+                print("No cached result...executing")
+            tic = dt.datetime.now()
+            result = func(*args, **kwargs)
+            time_taken = dt.datetime.now() - tic
+            if time_taken.seconds > threshold:
+                if isinstance(result, pd.DataFrame):
+                    fname = f"{func.__name__}_{key}.csv"
+                    result.to_csv(fname, index=index)
+                else:
+                    fname = f"{func.__name__}_{key}.h5"
+                    dd.io.save(fname, result, compression="zlib")
+                print(f"Exceeded threshold. Result cached to {fname}")
+            return result
+
+        return wrapper
+    return decorator
--- utilz/tests/test_guards.py
+++ utilz/tests/test_guards.py
@@ -1,17 +1,76 @@
 import pandas as pd
-from utilz.guards import log_df
+from utilz.guards import log_df, disk_cache
+from time import sleep
+from pathlib import Path
+import datetime as dt
+
+# Load iris dataset from Seaborn's data repo
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
+)
 
 
 def test_log_df(capsys):
-    # Load iris dataset from Seaborn's data repo
-    df = pd.read_csv(
-        "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
-    )
-
     @log_df
     def group_mean(df, grp_col, val_col):
         return df.groupby(grp_col)[val_col].mean().reset_index()
 
-    _ = group_mean(df, 'species', 'petal_length')
+    _ = group_mean(df, "species", "petal_length")
     captured = capsys.readouterr()
-    assert 'Func group_mean df shape=(3, 2)' in captured.out
+    assert "Func group_mean df shape=(3, 2)" in captured.out
+
+
+def test_disk_cache(tmp_path):
+    # Dataframe
+    @disk_cache(threshold=5, verbose=True)
+    def my_long_func(df, arg, my_kwarg1="hi", my_kwarg2=False):
+        sleep(6)
+        return df
+
+    key = "my_long_func_([2], (('my_kwarg1', 'hi'), ('my_kwarg2', False)))"
+    fpath = Path(f"{key}.csv")
+    # Remove files from failed tests
+    if fpath.exists():
+        fpath.unlink()
+    tic = dt.datetime.now()
+    _ = my_long_func(df, 2, my_kwarg1="hi", my_kwarg2=False)
+    dur1 = dt.datetime.now() - tic
+    dur1 = dur1.seconds
+    assert Path(fpath).exists()
+
+    tic = dt.datetime.now()
+    my_output = my_long_func(df, 2, my_kwarg1="hi", my_kwarg2=False)
+    dur2 = dt.datetime.now() - tic
+    dur2 = dur2.seconds
+    assert dur2 < dur1
+    assert my_output.equals(df)
+    # Remove files from successful tests
+    fpath.unlink()
+
+    # Other dtypes
+    @disk_cache(threshold=5, verbose=True)
+    def my_long_func(x, arg):
+        sleep(6)
+        return x
+
+    key = "my_long_func_([True], ())"
+    fpath = Path(f"{key}.h5")
+
+    # Remove files from failed tests
+    if fpath.exists():
+        fpath.unlink()
+    tic = dt.datetime.now()
+    my_input = [1, 2, 3]
+    _ = my_long_func(my_input, True)
+    dur1 = dt.datetime.now() - tic
+    dur1 = dur1.seconds
+    assert Path(fpath).exists()
+
+    tic = dt.datetime.now()
+    my_output = my_long_func(my_input, True)
+    dur2 = dt.datetime.now() - tic
+    dur2 = dur2.seconds
+    assert dur2 < dur1
+    assert my_output == my_input
+    # Remove files from successful tests
+    fpath.unlink()
