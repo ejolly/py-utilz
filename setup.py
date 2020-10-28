f690a0886d8fce7e8235a92b8b5f007af8d05c6d
ejolly <eshin.jolly@gmail.com>
2020-10-28 02:33:47 -0400

    Added and configure travis.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
-extra_setuptools_args = dict(tests_require=["pytest", "pandas"])
+extra_setuptools_args = dict(tests_require=["pytest", "pytest-cov"])
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
-    
+
 with open("utilz/version.py") as f:
-    exec(f.read())
+    version = f.read()
 
 setup(
     name="utilz",
-    version=__version__,
+    version=version,
     author="Eshin Jolly",
     author_email="eshin.jolly@gmail.com",
     install_requires=requirements,
@@ -18,7 +18,7 @@ setup(
     license="MIT",
     description="Faster, easier, more robust python data analysis",
     long_description="Faster, easier, more robust python data analysis",
-    keywords=["functional-programming", "pipes","defensive data analysis"],
+    keywords=["functional-programming", "pipes", "defensive data analysis"],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Operating System :: OS Independent",
