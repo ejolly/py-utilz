4d5f1347cd8f2c3516fce4c5ea5d2e08bd8fbd57
ejolly <eshin.jolly@gmail.com>
2020-10-28 23:49:33 -0400

    Added doc strings to plot.

--- utilz/plot.py
+++ utilz/plot.py
@@ -6,7 +6,17 @@ __all__ = ["setup"]
 
 
 def setup(figsize=(8, 6), subplots=(1, 1)):
-    import matplotlib.pyplot as plt
+    """
+    Setup matplotlib subplots boilerplate
 
+    Args:
+        figsize (tuple, optional): Figure size. Defaults to (8, 6).
+        subplots (tuple, optional): subplot grid size. Defaults to (1, 1).
+
+    Returns:
+        tuple ((Figure, Axes)): matplotlib figure handle and axes
+    """
+    if "plt" not in dir():
+        import matplotlib.pyplot as plt
     f, ax = plt.subplots(*subplots, figsize=figsize)
     return f, ax
