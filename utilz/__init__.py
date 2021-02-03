bf8d60ac2c4508f6d89daa7020a219afd83aa6ce
ejolly <eshin.jolly@gmail.com>
2021-02-02 23:36:33 -0500

    New boilerplate.parseargs function. Added python vscode settings.

--- utilz/__init__.py
+++ utilz/__init__.py
@@ -2,6 +2,6 @@ from .pipe import Pipe
 from .guards import log, time, maybe, disk_cache, same_shape, same_nunique
 from .ops import norm_by_group, pmap, prep, random_seed, mapcat, splitdf
 from .io import load, save, nbsave, nbload, clear_cache
-from .boilerplate import mpinit, randdf
+from .boilerplate import mpinit, randdf, parseargs
 from .verbs import groupby, rows, cols, rename, save, summarize, assign, apply
 from .version import __version__
--- utilz/boilerplate.py
+++ utilz/boilerplate.py
@@ -2,10 +2,11 @@
 Functions that exist purely to save boilerplate.
 """
 
-__all__ = ["mpinit", "randdf"]
+__all__ = ["mpinit", "randdf", "parseargs"]
 
 import numpy as np
 import pandas as pd
+import argparse
 
 
 def mpinit(figsize=(8, 6), subplots=(1, 1)):
@@ -45,3 +46,47 @@ def randdf(size=(10, 3), columns=["A", "B", "C"], func=None, *args, **kwargs):
         data = func(*args, size=size, **kwargs)
 
     return pd.DataFrame(data, columns=columns)
+
+
+# TODO: Test me and check out how docstring looks
+def parseargs(*arg_tuples: list) -> dict:
+    """
+    Quickly setup and use an argument parser to parse command line arguments. Useful as a one-liner added to the top of an interactive script or notebook to quickly make it useable at the command line instead. Desired arguments should be passed in as tuples containing 3 values:
+
+        (argument name as a string, argument type/True or False, argument help string)
+
+    The outputted dictionary will contain keys as the argument names, and values as the inputted values. Argument names will auto-convert '-' to '_' and strip any '--' prefixes.
+
+    To create positional command-line inputs the second tuple element should be a python type:
+
+    >>> parsed_args = arparser(('name', str, 'the username to print'))
+
+    >>> parsed_args['name'] # to retreive the value which will be a string
+
+    For boolean flags the first element should begin with '--' prefix, and second element should True or False:
+
+    >>> parsed_args = argparser(('--dry-run', True, 'setup the script but dont execute')
+
+    >>> parsed_args['dry_run'] # True
+
+    Returns:
+        dict: a dictionary containing keys for the argument names and values as the captured inputs
+    """
+
+    parser = argparse.ArgumentParser()
+
+    for tup in arg_tuples:
+
+        arg_name, arg_type, arg_help = tup
+
+        if isinstance(arg_type, type):
+            # User is defining the type of argument, e.g. str
+            parser.add_argument(arg_name, type=arg_type, help=arg_help)
+
+        elif isinstance(arg_type, bool):
+            # User is passing in True or False so convert to store_true/store_false
+            assert arg_name.startswith("--"), "Prefix boolean flags with '--'"
+            action_dict = {True: "store_true", False: "store_false"}
+            parser.add_argument(arg_name, action=action_dict[arg_type], help=arg_help)
+
+    return parser.parse_args()
