40f4eab08ee2a4444b36b9cd5c9182c713b5d7f3
ejolly <eshin.jolly@gmail.com>
2021-02-13 19:52:05 -0500

    More flexible auto-column naming in randdf

--- utilz/boilerplate.py
+++ utilz/boilerplate.py
@@ -7,6 +7,9 @@ __all__ = ["mpinit", "randdf", "parseargs"]
 import numpy as np
 import pandas as pd
 import argparse
+from typing import Union
+import string
+from itertools import cycle
 
 
 def mpinit(figsize: tuple = (8, 6), subplots: tuple = (1, 1)):
@@ -28,29 +31,38 @@ def mpinit(figsize: tuple = (8, 6), subplots: tuple = (1, 1)):
 
 def randdf(
     size: tuple = (10, 3),
-    columns: list = ["A", "B", "C"],
+    columns: Union[list, None] = None,
     func=None,
     *args: any,
-    **kwargs: any
+    **kwargs: any,
 ):
     """
     Generate a dataframe with random data and alphabetic columns, Default to np.random.randn. Specify another function and size will be passed in as a kwarg.
 
     Args:
         size (tuple, optional): Defaults to (10,3).
-        columns (list, optional): Defaults to ["A","B","C"].
+        columns (list, optional): Defaults numbered capital letters like an excel spreadsheet
         func (callable, optional): function to generate data. Must take a kwarg "size" that accepts a tuple; Default np.random.randn
         *args: positional arguments to func
         **kwargs: keyword arguments to func
     """
 
-    if len(columns) != size[1]:
+    if columns is not None and len(columns) != size[1]:
         raise ValueError("Length of column names must match number of columns")
 
     if func is None:
         data = np.random.rand(*size)
     else:
         data = func(*args, size=size, **kwargs)
+
+    if columns is None:
+        letters = cycle(string.ascii_uppercase)
+        counter = 1
+        columns = []
+        for i in range(size[1]):
+            if i > 0 and i % 26 == 0:
+                counter += 1
+            columns.append(f"{next(letters)}{counter}")
 
     return pd.DataFrame(data, columns=columns)
 
--- utilz/tests/test_boilerplate.py
+++ utilz/tests/test_boilerplate.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 from utilz.boilerplate import randdf
 
-five_col = ["A", "B", "C", "D", "E"]
-
 
 def test_randdf():
     df = randdf()
     assert isinstance(df, pd.DataFrame)
     assert df.shape == (10, 3)
-    assert all((a == b for a, b in zip(df.columns, ["A", "B", "C"])))
+    assert all((a == b for a, b in zip(df.columns, ["A1", "B1", "C1"])))
+
+    # Custom columns
+    five_col = ["A", "B", "C", "D", "E"]
 
     df = randdf(
         size=(20, 5),
