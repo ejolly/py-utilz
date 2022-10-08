78373e02dd496fb4ee25fbd3cb237e8ce48d143d
ejolly <eshin.jolly@gmail.com>
2022-10-07 23:35:20 -0400

    being dumb

--- utilz/__init__.py
+++ utilz/__init__.py
@@ -4,4 +4,3 @@ from .io import load, crawl
 from .boilerplate import randdf
 from .dftools import norm_by_group, assert_balanced_groups, assert_same_nunique, select
 from .plot import mpinit, stripbarplot, savefig
-from .version import __version__
