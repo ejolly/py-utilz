a409678f6282a9afeefdca39ca31e9ff56a60883
ejolly <eshin.jolly@gmail.com>
2023-06-17 21:33:07 -0400

    new isempty, checkany, checkall, and updated equal. Tests for shorts

--- utilz/shorts.py
+++ utilz/shorts.py
@@ -1,10 +1,13 @@
 """Shorthands for common operations or ops with specific params set"""
 
-__all__ = ["keep", "discard", "seq", "equal"]
+__all__ = ["keep", "discard", "seq", "equal", "isempty", "checkany", "checkall"]
 
-from .maps import filter
+from collections.abc import Iterable
+from .maps import filter, map
 from .ops import curry
 from toolz import diff
+import pandas as pd
+import numpy as np
 
 
 @curry
@@ -27,7 +30,22 @@ def seq(n):
 
 
 def equal(*seqs):
-    """Lazily checks if two sequences of potentionally different lengths are equal"""
+    """
+    Checks if N args of potentionally different lengths are equal.
+    Non-iterable args are directly compared with `==`
+    Dataframes and arrays use `.equals()` and `np.allclose()` respectively
+    """
+
+    if not isinstance(seqs[0], Iterable):
+        return checkall(lambda e: e == seqs[0], seqs)
+
+    if isinstance(seqs[0], pd.DataFrame):
+        return checkall(lambda e: e.equals(seqs[0]), seqs)
+
+    if isinstance(seqs[0], np.ndarray):
+        return checkall(lambda e: np.allclose(e, seqs[0]), seqs)
+
+    # For other sequence types we can be lazy
     return not any(diff(*seqs, default=object()))
 
 
@@ -39,3 +57,52 @@ def aslist(e):
 def asstr(e):
     """Idempotently convert something to a str."""
     return str(e) if not isinstance(e, str) else e
+
+
+def isempty(iterme: Iterable):
+    """Check if iterable is empty"""
+    return len(iterme) == 0
+
+
+@curry
+def checkany(func, iterme, transparent=False):
+    """
+    Check if any elements are func(elem) == True
+
+    Args:
+        func (callable): function that returns True or False
+        iterme (iterable): iterable
+        transparent (bool, optional): return iterme instead of result if check passes, useful in pipes; Default False
+
+    Returns:
+        True or False
+    """
+
+    result = any(map(func, iterme))
+    if transparent and result:
+        return iterme
+    if transparent and not result:
+        raise ValueError("Check failed")
+    return result
+
+
+@curry
+def checkall(func, iterme, transparent=False):
+    """
+    Check if all elements are func(elem) == True
+
+    Args:
+        func (callable): function that returns True or False
+        iterme (iterable): iterable
+        transparent (bool, optional): return iterme instead of result if check passes, useful in pipes; Default False
+
+    Returns:
+        True or False
+    """
+
+    result = all(map(func, iterme))
+    if transparent and result:
+        return iterme
+    if transparent and not result:
+        raise ValueError("Check failed")
+    return result
---
+++ utilz/tests/test_shorts.py
@@ -0,0 +1,64 @@
+from utilz.shorts import checkany, checkall, seq, equal, isempty, equal
+from utilz.ops import pipe
+import pandas as pd
+import numpy as np
+from utilz.boilerplate import randdf
+import pytest
+
+
+def test_checks():
+    # Basic boolean check
+    l = seq(10)
+    assert checkany(lambda x: x < 5, l)
+    assert not checkall(lambda x: x < 5, l)
+    assert checkall(lambda x: x < 10, l)
+
+    # Currying
+    assert pipe(l, checkany(lambda x: x < 5))
+    assert not pipe(l, checkall(lambda x: x < 5))
+    assert pipe(l, checkany(lambda x: x < 10))
+
+    # Transparency in pips returns the result if check passes
+    assert equal(l, pipe(l, checkany(lambda x: x < 5, transparent=True)))
+    assert equal(l, pipe(l, checkany(lambda x: x < 10, transparent=True)))
+
+    # Otherwise raises value error
+    with pytest.raises(ValueError):
+        pipe(l, checkall(lambda x: x < 5, transparent=True))
+
+
+def test_isempty():
+    assert isempty(pd.DataFrame())
+    assert not isempty(randdf())
+
+    assert isempty(np.array([]))
+    assert not isempty(np.arange(10))
+
+    assert isempty(dict())
+    assert not isempty({"name": "name"})
+
+
+def test_equal():
+    # Compare scalars
+    x, y = 2, 2
+    assert equal(x, y)
+
+    # And strings
+    x, y = "hi", "bye"
+    assert not equal(x, y)
+
+    # Compare dataframes
+    assert not equal(randdf(), randdf())
+
+    df = randdf()
+    assert equal(df, df.copy())
+
+    # Compare numpy arrays
+    data = [np.arange(10).astype(float) for i in range(4)]
+    # We can compare as many items as we want, equivalent to:
+    # equal(data[0], data[1], data[2])
+    assert equal(*data)
+
+    # numpy comparisons are made using np.allclose
+    data[0] += 0.00001
+    assert not equal(*data)
